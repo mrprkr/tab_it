81b07b07a64e642b3d88715e0b392c507745a3cb
Michael Parker <parker_michael@me.com>
2015-04-06 12:24:59 +1000

    Update 2015-04-06T02:24:37.575Z

--- assets/js/angular-app.js
+++ assets/js/angular-app.js
@@ -1,4 +1,5 @@
 var data = data;
+var transactions = transactions;
 
 var app = angular.module('app', ['ngRoute', 'templatescache']);
 
@@ -29,14 +30,60 @@ app.directive('focusInput', function($timeout) {
   };
 });
 
-app.controller('homepage-controller', function($scope, $http, $location, $timeout){
-	$scope.test = "hello world";
+app.filter('dateSuffix', function($filter) {
+  var suffixes = ["th", "st", "nd", "rd"];
+  return function(input) {
+    var dtfilter = $filter('date')(input, 'MMMM dd');
+    var day = parseInt(dtfilter.slice(-2));
+    var relevantDigits = (day < 30) ? day % 20 : day % 30;
+    var suffix = (relevantDigits <= 3) ? suffixes[relevantDigits] : suffixes[0];
+    return dtfilter+suffix;
+  };
+});
 
-	$scope.balance = 13.40;
+app.controller('homepage-controller', function($scope, $http, $location, $timeout){
+	// $scope.balance = 13.40;
 	$scope.formState = "collapsed";
+
+
+	$scope.user = {
+		_id : Number,
+		name: String,
+		email: String
+	}
+	$scope.user = {
+			_id: 12345,
+			name: "Michael",
+			email: "p88@me.com"
+		};
+
+	$scope.transactions = transactions;
+
+	$scope.findBalance = function(){
+		var yourExpenses = 0;
+		var yourLiabilites = 0;
+		for(t in $scope.transactions){
+			if($scope.transactions[t].payer._id === $scope.user._id){
+				yourExpenses += $scope.transactions[t].amount*$scope.transactions[t].split;	
+			}
+			else{
+				yourLiabilites += $scope.transactions[t].amount*$scope.transactions[t].split;
+			}
+		}
+		console.log("Your liabilities are: "+yourLiabilites);
+		console.log("Your expenses are: "+yourExpenses);
+		$scope.balance = yourExpenses-yourLiabilites;
+		console.log("Tab balance is :"+$scope.balance);
+	}
+
+	$scope.$watchCollection('transactions', function(){
+		$scope.findBalance();
+	})
+
 
 	$scope.toggleExpand = function(){
 		if($scope.formState === "collapsed"){
+			$scope.newTransaction = {};
 			$scope.formState = "expanded";
 			$scope.createButton = {'border':'2px solid #fff', 'cursor':'default'};
 		}
@@ -46,9 +93,22 @@ app.controller('homepage-controller', function($scope, $http, $location, $timeou
 		}
 	}
 
+	$scope.clearDollars = function(){
+		if($scope.newTransaction.amount){
+			$scope.newTransaction.amount = $scope.newTransaction.amount.replace("$", "");
+		}
+	}
+
 	$scope.addExpense = function(){
 		$scope.formState = "collapsed"
 		$scope.createButton = {'border':'2px solid #111','cursor':'pointer'};
+		var randomID = Math.floor((Math.random() * 5000) + 1);
+		$scope.newTransaction._id = randomID;
+		$scope.newTransaction.amount = $scope.newTransaction.amount.replace("$", "");
+		$scope.newTransaction.payer = $scope.user;
+		$scope.newTransaction.split /= 100;
+		$scope.newTransaction.date = Date.now();
+		$scope.transactions.push($scope.newTransaction);
 	}
 
 	$scope.resetBalance = function(){
--- assets/js/data.js
+++ assets/js/data.js
@@ -27,3 +27,50 @@ var data = {
 
 	]
 }
+
+var transactions = [
+	{
+		_id : 123,
+		desc : "Shoping",
+		amount: 44,
+		split: 0.5,
+		date: 1428284341561,
+		payer: {
+			_id: 12345,
+			name: "Michael",
+			email: "p88@me.com"
+		}
+	},
+	{
+		_id : 456,
+		desc : "Beer",
+		amount: 23,
+		split: 0.5,
+		date: 1288323623006,
+		payer: {
+			_id: 67890,
+			name: "Tom",
+			email: "tom@me.com"
+		}
+	},
+	{
+		_id : 789,
+		desc : "Birthday dinner and drinks",
+		amount: 34.80,
+		split: 0.33,
+		date: 1428283610653,
+		payer: {
+			_id: 12345,
+			name: "Michael",
+			email: "p88@me.com"
+		}
+	}
+]
+
+var transaction = {
+	_id : Number,
+	desc : String,
+	amount: Number,
+	split: Number,
+	payer: Object
+}
