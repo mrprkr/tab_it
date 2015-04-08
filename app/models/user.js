4170fa4451e76016a18f6b5a932f5ed7b540dad2
Michael Parker <parker_michael@me.com>
2015-04-08 11:40:20 +1000

    fixed jade errors

--- app/models/user.js
+++ app/models/user.js
@@ -4,8 +4,10 @@ var Schema       = mongoose.Schema;
 var UserSchema = new Schema({
 	name: String,
 	email: String,
-	password: String,
-	token: String
+	password: String, //this is hashed
+	token: String,
+	loginAttempts: {type: Number, default: 0},
+	locked: {type: Boolean, default: false}
 })
 
 module.exports = mongoose.model('User', UserSchema);
